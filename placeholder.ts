93f4fa71987c4951d169a893fd2ad346e1e2d794
Scott Macmillan <CommanderTso@users.noreply.github.com>
2024-07-07 07:16:55 -0400

    Configure repo for VS Code linting

---
+++ placeholder.ts
@@ -0,0 +1,3 @@
+// This file exists to stop the linter from complaining
+// that there is no TS entrypoint
+// https://docs.joshuatz.com/cheatsheets/typescript/#error-no-inputs-were-found-in-config-file-when-using-glob-patterns
