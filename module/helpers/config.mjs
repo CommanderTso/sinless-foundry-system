450ae8c3c4a6a2bbea0a0f1a78f12d9067dc763c
Scott Macmillan <CommanderTso@users.noreply.github.com>
2024-07-15 12:52:43 -0400

    Finish implementation of SINLESS.round()

--- module/helpers/config.mjs
+++ module/helpers/config.mjs
@@ -15,8 +15,10 @@ SINLESS.attributes = {
 };
 
 SINLESS.round = (x) => {
-  if (typeof(x) === "number") {
-    return "rounded!"
+  if (typeof(x) === "number" && x >= 0) {
+    return (x > 0) && (x < 1) ? 1 : Math.floor(x)
+  } else if (typeof(x) === "number" && x < 0){
+    throw new Error("SINLESS.round is not designed to handle negative numbers.")
   } else {
     throw new Error("Tried to round something that was not a number.")
   }
