5757b64e4a189c2710637ba48176fd5ff36967a1
Scott Macmillan <CommanderTso@users.noreply.github.com>
2024-07-14 22:03:48 -0400

    Rip out more D&D style abilities stuff

--- module/documents/actor.mjs
+++ module/documents/actor.mjs
@@ -48,10 +48,10 @@ export class SinlessActor extends Actor {
     const systemData = actorData.system;
 
     // Loop through ability scores, and add their modifiers to our sheet output.
-    for (let [key, ability] of Object.entries(systemData.abilities)) {
-      // Calculate the modifier using d20 rules.
-      ability.mod = Math.floor((ability.value - 10) / 2);
-    }
+    // for (let [key, ability] of Object.entries(systemData.abilities)) {
+    //   // Calculate the modifier using d20 rules.
+    //   ability.mod = Math.floor((ability.value - 10) / 2);
+    // }
   }
 
   /**
@@ -87,11 +87,11 @@ export class SinlessActor extends Actor {
 
     // Copy the ability scores to the top level, so that rolls can use
     // formulas like `@str.mod + 4`.
-    if (data.abilities) {
-      for (let [k, v] of Object.entries(data.abilities)) {
-        data[k] = foundry.utils.deepClone(v);
-      }
-    }
+    // if (data.abilities) {
+    //   for (let [k, v] of Object.entries(data.abilities)) {
+    //     data[k] = foundry.utils.deepClone(v);
+    //   }
+    // }
 
     // Add level for easier access, or fall back to 0.
     // if (data.attributes.level) {
--- module/helpers/config.mjs
+++ module/helpers/config.mjs
@@ -14,24 +14,6 @@ SINLESS.attributes = {
   charisma:     'SINLESS.Attributes.Charisma',
 };
 
-SINLESS.abilities = {
-  str: 'SINLESS.Ability.Str.long',
-  dex: 'SINLESS.Ability.Dex.long',
-  con: 'SINLESS.Ability.Con.long',
-  int: 'SINLESS.Ability.Int.long',
-  wis: 'SINLESS.Ability.Wis.long',
-  cha: 'SINLESS.Ability.Cha.long',
-};
-
-SINLESS.abilityAbbreviations = {
-  str: 'SINLESS.Ability.Str.abbr',
-  dex: 'SINLESS.Ability.Dex.abbr',
-  con: 'SINLESS.Ability.Con.abbr',
-  int: 'SINLESS.Ability.Int.abbr',
-  wis: 'SINLESS.Ability.Wis.abbr',
-  cha: 'SINLESS.Ability.Cha.abbr',
-};
-
 SINLESS.round = (x) => {
   if (typeof(x) === "number") {
     return "rounded!"
