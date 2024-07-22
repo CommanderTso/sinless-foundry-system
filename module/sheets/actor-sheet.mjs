223245f0e83297d0e366c62cf211f6c7de2d3e88
Scott Macmillan <CommanderTso@users.noreply.github.com>
2024-07-22 16:14:05 -0400

    Update rolls to do basic Sinless pool check

--- module/sheets/actor-sheet.mjs
+++ module/sheets/actor-sheet.mjs
@@ -237,7 +237,7 @@ export class SinlessActorSheet extends ActorSheet {
 
     // Handle rolls that supply the formula directly.
     if (dataset.roll) {
-      let label = dataset.label ? `[ability] ${dataset.label}` : ''
+      let label = dataset.label ? `[pool] ${dataset.label}` : ''
       let roll = new Roll(dataset.roll, this.actor.getRollData())
       roll.toMessage({
         speaker: ChatMessage.getSpeaker({ actor: this.actor }),
